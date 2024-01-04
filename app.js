7ef98732f96b52fa72afaa8e833e8185afe72fbe
avnichauhan12 <vidchauhan9182@gmail.com>
2024-01-04 23:37:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let names=prompt("Enter your name");
+console.log(names);
+str=names.trim();
+console.log(str);
+nameUpper=names.toUpperCase();
+nameLower=names.toLowerCase();
+console.log(nameUpper);
+console.log(nameLower);
+alert("Happy new year");
